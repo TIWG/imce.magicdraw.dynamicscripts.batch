af6d749ab198c65699c1b59650b54f6375cef775
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-06-14 22:23:41 +0000

    IMCEI-186 - Build a beta release of the OMF exporter dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project
-  val version = "2.16.0"
+  val version = "2.17.0"
 
   // https://github.com/spray/spray-json
   val spray_json = "1.3.2"
--- project/Versions_imce_oti_mof_magicdraw_dynamicscripts.scala
+++ project/Versions_imce_oti_mof_magicdraw_dynamicscripts.scala
@@ -1,6 +1,6 @@
 object Versions_imce_oti_mof_magicdraw_dynamicscripts {
 
   // https://github.jpl.nasa.gov/imce/imce.oti.mof.magicdraw.dynamicscripts
-  val version="2.12.0"
+  val version="2.13.0"
 
 }
