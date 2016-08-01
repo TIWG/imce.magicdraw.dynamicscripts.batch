2ed2d3e2230c96d6158229d9806980131a4e39c1
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-08-01 16:26:17 +0000

    IMCEI-272 - Add connector to dump OTI MOF profiles/models for SPARK dependencies

--- project/Versions_imce_oti_mof_magicdraw_dynamicscripts.scala
+++ project/Versions_imce_oti_mof_magicdraw_dynamicscripts.scala
@@ -1,6 +1,6 @@
 object Versions_imce_oti_mof_magicdraw_dynamicscripts {
 
   // https://github.jpl.nasa.gov/imce/imce.oti.mof.magicdraw.dynamicscripts
-  val version="3.4.0"
+  val version="3.5.0"
 
 }
